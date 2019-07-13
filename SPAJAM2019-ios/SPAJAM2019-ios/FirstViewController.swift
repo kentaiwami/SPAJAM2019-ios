5146ecd7a4d29c2f42d6abf2588d54489b349baf
Kenta Iwami <kentaiwami12@gmail.com>
2019-07-13 16:14:36 +0900

    first commit

---
+++ SPAJAM2019-ios/SPAJAM2019-ios/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SPAJAM2019-ios
+//
+//  Created by 岩見建汰 on 2019/07/13.
+//  Copyright © 2019 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SPAJAM2019-ios/SPAJAM2019-ios/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SPAJAM2019-ios
+//
+//  Created by 岩見建汰 on 2019/07/13.
+//  Copyright © 2019 Kenta. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
