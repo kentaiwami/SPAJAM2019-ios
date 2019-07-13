6ad7ef10a5307391db7250bda5448d0e0bcbc317
Kenta Iwami <kentaiwami12@gmail.com>
2019-07-13 16:21:13 +0900

    add entity

---
+++ SPAJAM2019-ios/SPAJAM2019-ios/Entity.swift
@@ -0,0 +1,15 @@
+//
+//  Entity.swift
+//  SPAJAM2019-ios
+//
+//  Created by 岩見建汰 on 2019/07/13.
+//  Copyright © 2019 Kenta. All rights reserved.
+//
+
+import Foundation
+
+struct Data {
+    var id: Int
+    var date: Date
+    var image: String
+}
